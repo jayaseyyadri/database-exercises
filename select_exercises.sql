ad31e8b76f8f5fe5e70df4510c5cfc73fdb8795a
jaya <jayaseyyadri@gmail.com>
2021-01-11 16:18:58 -0600

    Basic Statements exercise

---
+++ select_exercises.sql
@@ -0,0 +1,22 @@
+USE codeup_test_db;
+
+Select 'Pink Floyds collections' as '';
+SELECT * FROM albums where artist ='Pink Floyd';
+
+
+Select 'Year of Release' as '';
+SELECT RELEASE_DATE FROM ALBUMS WHERE NAME ='Sgt. Pepper''s Lonely Hearts Club Band';
+
+
+Select 'GENRE' as '';
+SELECT genre FROM albums WHERE genre='Nevermind';
+
+Select '1990 ALBUMS' as '';
+SELECT * FROM albums WHERE release_date=1990;
+
+Select 'SALES<20 MILLION' as '';
+SELECT * FROM albums WHERE sales<20.0;
+
+
+Select 'GENRE --ROCK' as '';
+SELECT * FROM albums WHERE genre='Rock';
