be8055e7780f76445e490edbedbb4020b8a0ffa6
jaya <jayaseyyadri@gmail.com>
2021-01-14 16:10:40 -0600

    finished indexes  exercises

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,3 +1,13 @@
 USE codeup_test_db;
 
+DROP TABLE IF EXISTS albums;
 
+CREATE TABLE albums(
+    id INT(5) UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50) NOT NULL DEFAULT 'Unknown' ,
+    name VARCHAR(100)NOT NULL DEFAULT 'Untitled',
+    release_date INT UNSIGNED,
+    sales FLOAT (20) UNSIGNED,
+    genre CHAR(40),
+    PRIMARY KEY (id)
+);
